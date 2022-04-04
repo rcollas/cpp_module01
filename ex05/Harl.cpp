e11e8d304de581f5c552c0847e3c0fd1ad6a5ec9
ModXPal <collas.robin@gmail.com>
2022-04-04 11:07:14 +0200

    using switch in ex06

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,6 +6,7 @@ Harl::~Harl() {}
 
 void Harl::debug() const {
 
+	std::cerr << "[DEBUG]" << std::endl;
 	std::cerr << "I love having extra bacon for my "
 				 "7XL-double-cheese-triple-pickle-special-ketchup"
 				 " burger. I really do!" << std::endl;
@@ -13,6 +14,7 @@ void Harl::debug() const {
 
 void Harl::info() const {
 
+	std::cerr << "[INFO]" << std::endl;
 	std::cerr << "I cannot believe adding extra bacon costs more money."
 				 " You didn’t put enough bacon in my burger! "
 				 "If you did, I wouldn’t be asking"
@@ -21,6 +23,7 @@ void Harl::info() const {
 
 void Harl::warning() const {
 
+	std::cerr << "[WARNING]" << std::endl;
 	std::cerr << "I think I deserve to have some extra bacon for free. "
 				 "I’ve been coming for years whereas you started "
 				 "working here since last month." << std::endl;
@@ -28,6 +31,7 @@ void Harl::warning() const {
 
 void Harl::error() const {
 
+	std::cerr << "[ERROR]" << std::endl;
 	std::cerr << "This is unacceptable! "
 				 "I want to speak to the manager now."
 				 << std::endl;
